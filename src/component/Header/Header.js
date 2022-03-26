0bfbd8eaaff36cd640e12110bcab476734dd85e9
uday-g-ithub15 <mudayhasan@gmail.com>
2022-03-26 08:10:27 +0600

    First-and-Navbar

---
+++ src/component/Header/Header.js
@@ -0,0 +1,17 @@
+import './Header.css'
+
+const Header = () => {
+    
+    return (
+        <>
+            <header>
+                <nav>
+                    <h3>Buy your <span>Spectacles</span> from us</h3>
+                </nav>
+            </header>
+            
+        </>
+    );
+};
+
+export default Header;
