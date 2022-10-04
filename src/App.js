508b763fb62263d80a006d44974759799dccd326
uday-g-ithub15 <mudayhasan@gmail.com>
2022-10-05 02:25:40 +0600

    remove blogs and add width to body container

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import Glasses from './component/Glasses/Glasses';
 import Header from './component/Header/Header';
-import Questions from './component/Questions/Questions';
 
 function App() {
   return (
     <>
-<Header></Header>
-<Glasses></Glasses>
-<Questions></Questions>
+      <Header></Header>
+      <Glasses></Glasses>
     </>
   );
 }
