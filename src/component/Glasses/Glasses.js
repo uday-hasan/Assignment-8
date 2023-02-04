c8d68d62ba2b79311a2414d8fc1459021aa3018c
uday-g-ithub15 <mudayhasan@gmail.com>
2023-02-04 18:38:56 +0600

    some changes

--- src/component/Glasses/Glasses.js
+++ src/component/Glasses/Glasses.js
@@ -1,62 +1,66 @@
-import React,  { useState,useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import Cart from '../Cart/Cart';
 import Glass from '../Glass/Glass';
 import './Glasses.css';
 
 const Glasses = () => {
     const [products, setproducts] = useState([]);
-    const [carted,setCarted] = useState([]);
+    const [carted, setCarted] = useState([]);
     useEffect(() => {
         fetch('data.json').then(res => res.json()).then(data => setproducts(data))
-    },[])
-    const addToCart = (element) =>{
-        const newCarted = [...carted,element]
-        if(carted.indexOf(element) !== -1){
+    }, [])
+    const addToCart = (element) => {
+        const newCarted = [...carted, element]
+        if (carted.indexOf(element) !== -1) {
             alert('Already Exist')
         }
-        else{
-            if(carted.length > 3){
-             alert('You can selete only 4 items')   
+        else {
+            if (carted.length > 3) {
+                alert('You can selete only 4 items')
             }
-            else{
+            else {
                 setCarted(newCarted);
             }
         }
-  }
-        const removeCart = element => {
-            const rest = carted.filter(product => product !== element);
-            setCarted(rest);
+    }
+    const removeCart = element => {
+        const rest = carted.filter(product => product !== element);
+        setCarted(rest);
+    }
+    const clearCart = () => {
+        setCarted([]);
+    }
+    const randomProduct = () => {
+        if (carted.length <= 0) {
+            alert('There have no sufficient element for choose one');
         }
-        const clearCart = () => {
-            setCarted([]);
+        else {
+            const hello = Math.floor(Math.random() * carted.length);
+            const selectedProduct = [carted[hello]]
+            setCarted(selectedProduct)
+            console.log(selectedProduct);
         }
-        const randomProduct = () => {
-            if(carted.length <= 0){
-                alert('There have no sufficient element for choose one');
-            }
-            else{
-                const hello = Math.floor(Math.random() * carted.length);
-                const selectedProduct = [carted[hello]]
-                setCarted(selectedProduct)
-                console.log(selectedProduct);
-            }
-        }
+    }
     return (
-       <>
-          <div className='product-container'>
-          <div className='glasses'>
-            {products.map(glass => <Glass glass={glass} key = {glass.id} addToCart = {() => addToCart(glass)} ></Glass>)}
-        </div>
-        <div className="cart">
-                    <h2 style={{textAlign:'center'}}>Carted Products</h2>
-            {
-                carted.map(cart => <Cart cart={cart} key={cart.id} removeCart = {() => removeCart(cart)}></Cart>)
-            }
-            <button onClick={randomProduct}>Find one</button>
-            <button onClick={clearCart}>Clear cart</button>
-        </div>
-          </div>
-       </>
+        <>
+            <div className='product-container'>
+                <div className='glasses'>
+                    {products.map(glass => <Glass glass={glass} key={glass.id} addToCart={() => addToCart(glass)} ></Glass>)}
+                </div>
+                <div className="cart">
+                    {
+                        carted.length ? <><h2 style={{ textAlign: 'center' }}>Carted Products</h2>
+                            {
+                                carted.map(cart => <Cart cart={cart} key={cart.id} removeCart={() => removeCart(cart)}></Cart>)
+                            }
+                            <button onClick={randomProduct}>Find one</button>
+                            <button onClick={clearCart}>Clear cart</button></>
+                            :
+                            <h1>No items found in cart.</h1>
+                    }
+                </div>
+            </div>
+        </>
     );
 };
 
